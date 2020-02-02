34b638fd79f5c1427c184119554bde4ad71c654b
Paul Hansen <paul.c.hansen@gmail.com>
2020-02-02 10:29:20 -0800

    Add get_quota_codes and get_quota.  Black reformat.

--- ec2tools/ec2tools.py
+++ ec2tools/ec2tools.py
@@ -7,6 +7,7 @@ import re
 import sys
 import time
 
+
 def get(obj, expr):
     """
     Quick jsonpath query.
@@ -19,6 +20,7 @@ def get(obj, expr):
     """
     jsonpath_expr = parse(expr)
     return [match.value for match in jsonpath_expr.find(obj)]
+
 
 INSTANCE_ATTRIBUTES = [
     "ami_launch_index",
@@ -70,6 +72,7 @@ INSTANCE_ATTRIBUTES = [
     "vpc_id",
 ]
 
+
 def get_instance_attributes(instance):
     """
     Get attributes of instance as a dict.
@@ -81,8 +84,11 @@ def get_instance_attributes(instance):
     Returns:
         dict: attributes
     """
-    instance_attributes = dict([(key,instance.__getattribute__(key)) for key in INSTANCE_ATTRIBUTES])
+    instance_attributes = dict(
+        [(key, instance.__getattribute__(key)) for key in INSTANCE_ATTRIBUTES]
+    )
     return instance_attributes
+
 
 def get_instance_ids(name=None):
     """
@@ -94,16 +100,15 @@ def get_instance_ids(name=None):
         list: matching instance IDs
     """
     ec2 = boto3.client("ec2")
-    
+
     if name:
-        tags_dict = ec2.describe_tags(
-            Filters=[{"Name":"value", "Values":[name]}],
-        )
+        tags_dict = ec2.describe_tags(Filters=[{"Name": "value", "Values": [name]}],)
         instance_ids = get(tags_dict, "Tags.[*].ResourceId")
     else:
         instances_dict = ec2.describe_instances()
         instance_ids = get(instances_dict, "Reservations[*].Instances[*].InstanceId")
     return instance_ids
+
 
 def get_instances(name=None):
     """
@@ -115,15 +120,15 @@ def get_instances(name=None):
         list: matching instances
     """
     ec2_resource = boto3.resource("ec2")
-    
+
     instance_ids = get_instance_ids(name=name)
-    
+
     instances = [ec2_resource.Instance(instance_id) for instance_id in instance_ids]
     return instances
 
 
-
 USERNAME_DICT = {"^Amazon Linux.*": "ec2-user"}
+
 
 def get_instance_username(image_description):
     """
@@ -134,7 +139,7 @@ def get_instance_username(image_description):
     """
     if not isinstance(image_description, str):
         raise TypeError(f"image_description ({image_description}) should be str)")
-    
+
     for (pattern, username) in USERNAME_DICT.items():
         result = re.match(pattern, image_description)
         if result:
@@ -160,6 +165,7 @@ def get_instance_tags(instance, key):
             tags.append(tag["Value"])
     return tags
 
+
 def get_instance_name(instance):
     """
     Get name of instance.  Returns empty string if no name is given.
@@ -174,7 +180,8 @@ def get_instance_name(instance):
         return name_tags[0]
     else:
         return ""
-    
+
+
 def get_instance_ssh_config_items(instance, pem_dir_path):
     """
     Create dict of fields to put in SSH config file.
@@ -187,23 +194,86 @@ def get_instance_ssh_config_items(instance, pem_dir_path):
     """
     username = get_instance_username(instance.image.description)
     name = get_instance_name(instance)
-    
+
     identity_file = os.path.join(pem_dir_path, f"{instance.key_name}.pem")
-    
+
     # I can't have spaces in the host name.
     if name == "" or " " in name:
         host = instance.public_ip_address
     else:
         host = name
-    
+
     out_dict = {
         "Host": host,
         "Hostname": instance.public_dns_name,
         "User": username,
         "IdentityFile": identity_file,
     }
-    
+
     return out_dict
+
+
+def get_quota_codes():
+    """
+    Get all EC2 quota codes.
+    
+    Returns:
+        dict: QuotaName -> QuotaCode
+    """
+    quotas = boto3.client("service-quotas")
+    result = quotas.list_service_quotas(ServiceCode="ec2")
+    quota_names = get(result, "Quotas[*].QuotaName")
+    quota_codes = get(result, "Quotas[*].QuotaCode")
+    quota_code_dict = dict(zip(quota_names, quota_codes))
+    return quota_code_dict
+
+
+def get_quota(quota_name=None, quota_code=None, quota_field=None):
+    """
+    Get quota dict for a given EC2 service.
+    
+    Example of EC2 service: "Running On-Demand G instances".
+    Its quota code is "L-DB2E81BA".
+    
+    For full list of EC2 service quota codes, call get_quota_codes().
+    
+    Args:
+        quota_name (str): EC2 service name such as "Running On-Demand G instances"
+        quota_code (str): EC2 service quota code such as "L-DB2E81BA"
+        quota_field (str): Single field of quota dict such as "Value"
+    Returns:
+        service quota dict, or value of its quota_field field
+    """
+    if quota_name is not None:
+        if quota_code:
+            raise Exception("Only one of quota_name and quota_code should be provided.")
+        try:
+            quota_code = get_quota_codes()[quota_name]
+        except KeyError as exc:
+            msg = f"Unknown quota name '{quota_name}'.\n"
+            msg += "Valid quota names:\n\t" + "\n\t".join(get_quota_codes().keys())
+            raise Exception(msg) from None
+    elif quota_code is None:
+        raise Exception("Either quota_name or quota_code should be provided.")
+
+    quotas = boto3.client("service-quotas")
+    result = quotas.get_service_quota(ServiceCode="ec2", QuotaCode=quota_code)
+
+    try:
+        result = result["Quota"]
+    except KeyError as exc:
+        raise Exception("Service quota dict has no Quota key!")
+
+    if quota_field is not None:
+        try:
+            result = result[quota_field]
+        except KeyError as exc:
+            msg = f"Unknown quota field '{quota_field}'. "
+            msg += "Valid fields are " + ", ".join(result.keys()) + "."
+            raise Exception(msg) from None
+
+    return result
+
 
 def update_ssh_config(config_path, instances, pem_dir_path, new_config_path=None):
     """
@@ -222,23 +292,28 @@ def update_ssh_config(config_path, instances, pem_dir_path, new_config_path=None
         new_config_path (str, optional): path for writing config file
     """
     config_path = os.path.expanduser(config_path)
-    
+
     if new_config_path is None:
         new_config_path = config_path
     else:
         new_config_path = os.path.expanduser(config_path)
-    
+
     try:
         c = sshconf.read_ssh_config(config_path)
     except FileNotFoundError:
         raise
-    
+
     for instance in instances:
         items = get_instance_ssh_config_items(instance, pem_dir_path)
         if items["Host"] in c.hosts():
             c.remove(items["Host"])
-        c.add(items["Host"], Hostname=items["Hostname"], User=items["User"], IdentityFile=items["IdentityFile"])
-    
+        c.add(
+            items["Host"],
+            Hostname=items["Hostname"],
+            User=items["User"],
+            IdentityFile=items["IdentityFile"],
+        )
+
     c.write(new_config_path)
 
 
@@ -260,28 +335,28 @@ def wait_for_state(instance, desired_state, timeout=300, verbose=True):
         instances = instance
     else:
         instances = [instance]
-    
+
     sleep_count = 0
-    
+
     msg_len = 0
-    
+
     states = [instance.state["Name"] for instance in instances]
     while any([s != desired_state for s in states]):
         if verbose:
             msg = "\r" + ", ".join(states) + f" [{sleep_count} s]"
-            msg_len = max(len(msg), msg_len) # need for stupid line rewrite at end
+            msg_len = max(len(msg), msg_len)  # need for stupid line rewrite at end
             sys.stdout.write(msg)
         sleep_count += 1
         time.sleep(1)
-        
+
         for instance in instances:
             instance.reload()
-        
+
         states = [instance.state["Name"] for instance in instances]
-        
+
         if sleep_count > timeout:
             raise Exception(f"Timeout occurred (t > {timeout} s).")
-    
+
     if verbose:
-        sys.stdout.write("\r" + " "*msg_len)
+        sys.stdout.write("\r" + " " * msg_len)
         sys.stdout.write("\rDone.")
